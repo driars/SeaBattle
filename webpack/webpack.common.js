a93a7df7d4eefa430ad6e1381b334c69251098c5
driars <driars0329@gmail.com>
2023-08-25 22:05:47 +0200

    Chore: Correct format of webpack

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,12 +1,11 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWebPack = require("copy-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require('path')
 
 module.exports = {
-  entry: path.resolve(__dirname, "..", "./src/index.tsx"),
+  entry: path.resolve(__dirname, '..', './src/index.tsx'),
   resolve: {
-    extensions: [".js", ".ts", ".tsx"],
+    extensions: ['.js', '.ts', '.tsx'],
   },
   module: {
     rules: [
@@ -14,30 +13,30 @@ module.exports = {
         // should use babel-loader for all ts js tsx and jsx files
         test: /\.(ts|js)x?$/,
         exclude: /node_modules/,
-        use: [{ loader: "babel-loader" }],
+        use: [{ loader: 'babel-loader' }],
       },
       {
         test: /\.css$/,
-        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
       },
       {
         test: /\.(?:ico|gif|jpg|png|bmp)$/,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
       {
         test: /\.(?:woff(?:2)?|eot|ttf|otf|svg)$/,
-        type: "asset/inline",
+        type: 'asset/inline',
       },
     ],
   },
   output: {
-    path: path.resolve(__dirname, "..", "./build"),
-    filename: "[name].[chunkhash:8].js",
+    path: path.resolve(__dirname, '..', './build'),
+    filename: '[name].[chunkhash:8].js',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "..", "./src/index.html"),
+      template: path.resolve(__dirname, '..', './src/index.html'),
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
   ],
-};
+}
