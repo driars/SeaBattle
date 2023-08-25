2d610e53b709c39914e5a79ad84146301913a318
driars <driars0329@gmail.com>
2023-08-25 21:01:17 +0200

    Test Button click event

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from "react";
+import { ChatBox } from "./Components/ChatBox";
 
 export const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      App <ChatBox />
+    </div>
+  );
+};
---
+++ src/Components/ChatBox.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+export const ChatBox = () => {
+  const [count, setCount] = useState(0);
+
+  const onClick = () => setCount((count) => count + 1);
+
+  return (
+    <div>
+      <span>{count}</span>
+      <button onClick={onClick}>Increase Count</button>
+    </div>
+  );
+};
